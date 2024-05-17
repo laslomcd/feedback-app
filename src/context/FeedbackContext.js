aa07a21f7cb38b27ece08406a1c80db8d99997dd
ryan <laslomcd@gmail.com>
2024-05-17 17:15:20 -0400

    added spinner and added concurrently

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,32 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: "This is feedback item 1",
-			rating: 10,
-		},
-		{
-			id: 2,
-			text: "This is feedback item 2",
-			rating: 7,
-		},
-		{
-			id: 3,
-			text: "This is feedback item 3",
-			rating: 9,
-		},
-	]);
-
+	const [isLoading, setIsLoading] = useState(true);
+	const [feedback, setFeedback] = useState([]);
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
 	});
 
+	useEffect(() => {
+		fetchFeedback();
+	}, []);
+
+	// Fetch Feedback
+	const fetchFeedback = async () => {
+		const response = await fetch(`http://localhost:5500/feedback?_sort=id`);
+		const data = await response.json();
+
+		setFeedback(data);
+		setIsLoading(false);
+	};
+
+	// Delete Feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm("Are you sure you want to delete?")) {
 			setFeedback(feedback.filter((item) => item.id !== id));
@@ -35,7 +33,6 @@ export const FeedbackProvider = ({ children }) => {
 
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4();
-		console.log(newFeedback);
 		setFeedback([newFeedback, ...feedback]);
 	};
 
@@ -52,7 +49,7 @@ export const FeedbackProvider = ({ children }) => {
 
 	return (
 		<FeedbackContext.Provider
-			value={{ feedback, deleteFeedback, addFeedback, editFeedback, feedbackEdit, updateFeedback }}
+			value={{ feedback, deleteFeedback, addFeedback, editFeedback, feedbackEdit, updateFeedback, isLoading }}
 		>
 			{children}
 		</FeedbackContext.Provider>
